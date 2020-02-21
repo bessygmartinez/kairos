2ab1078b33b68ea56217d3548da0672ffa746c27
Bessy Gee <nakiwarai13@gmail.com>
2020-02-21 17:00:51 -0500

    ran npm run build

---
+++ client/build/precache-manifest.68b4a293c8868845af37e82457347ed6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3f38e666261cf7336f1",
+    "url": "/static/css/main.93fc0200.chunk.css"
+  },
+  {
+    "revision": "b3f38e666261cf7336f1",
+    "url": "/static/js/main.455ab3da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6765964e645a4a702152",
+    "url": "/static/js/2.60354bd6.chunk.js"
+  },
+  {
+    "revision": "125ff5c33776935517ced8d3e0e6d9bd",
+    "url": "/index.html"
+  }
+];
