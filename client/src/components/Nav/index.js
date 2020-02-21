909b2bd03abb63526d6664ebfffd3c8324d0dd82
Bessy Gee <nakiwarai13@gmail.com>
2020-02-21 14:37:08 -0500

    added kairos logo

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
+    <nav className="navbar navbar-expand-lg navbar-dark bg-light">
+      <a className="navbar-brand nav-link" href="/">
+        <img src="/img/kairos_logo.png" alt="kairos logo" className="img-fluid" style={{width: "30%"}}></img>
       </a>
     </nav>
   );
