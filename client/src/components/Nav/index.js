ee880ca23dd32b1505d28bfd46db88057d9368b3
Bessy Gee <nakiwarai13@gmail.com>
2020-02-21 21:11:07 -0500

    added preliminary navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-light">
-      <a className="navbar-brand nav-link" href="/">
-        <img src="/img/kairos_logo.png" alt="kairos logo" className="img-fluid" style={{width: "30%"}}></img>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a href="/" className="navbar-brand">
+      <img src="/img/kairos_logo.png" alt="kairos logo" className="d-inline-block" width="250vw"></img>
       </a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarmenu"
+      aria-controls="navbarmenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarmenu">
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item">
+        <Link to="/" className="nav-link">Home</Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/" className="nav-link">Link</Link>
+      </li>
+    </ul>
+  </div>
     </nav>
   );
 }
