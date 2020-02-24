f09e96eb68821c896ca20a4d047b7d3f1ca9f4b6
Bessy Gee <nakiwarai13@gmail.com>
2020-02-24 15:01:54 -0500

    added script for state changes for user login

---
+++ client/src/components/Auth/Login.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+  }
+onChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+onSubmit = e => {
+    e.preventDefault();
+const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+console.log(userData);
+  };
+render() {
+    const { errors } = this.state;
+return (
+      <div className="container">
+        <div style={{ marginTop: "3rem" }} className="row">
+          <div className="col-sm-8 offset-sm-2">
+
+            <div className="col-sm-12" style={{ paddingLeft: "11.250px" }}>
+              <h4>
+                <b>Login</b> with your company-provided credentials below
+              </h4>
+              <p className="grey-text text-darken-1">
+                Don't have an account? <Link to="/register">Register</Link>
+              </p>
+            </div>
+            <form noValidate onSubmit={this.onSubmit}>
+              <div className="form-group">
+              <div className="input-field col-sm-12">
+                <label htmlFor="email">Email</label>
+                <input
+                  onChange={this.onChange}
+                  value={this.state.email}
+                  error={errors.email}
+                  id="email"
+                  type="email"
+                  className="form-control"
+                />
+               </div>
+              </div>
+
+              <div className="form-group">
+              <div className="input-field col-sm-12">
+                <label htmlFor="password">Password</label>
+                <input
+                  onChange={this.onChange}
+                  value={this.state.password}
+                  error={errors.password}
+                  id="password"
+                  type="password"
+                  className="form-control"
+                />
+              </div>
+              </div>
+
+              <div className="col-sm-12" style={{ paddingLeft: "11.250px" }}>
+                <button
+                  style={{
+                    width: "150px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem"
+                  }}
+                  type="submit"
+                  className="btn btn-raised btn-large waves-effect waves-light hoverable teal-btn text-white"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Login;
