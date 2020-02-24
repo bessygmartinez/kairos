fb276b135fb60f1466233c9b1f4fd28fd4e92a4e
Bessy Gee <nakiwarai13@gmail.com>
2020-02-24 14:58:18 -0500

    added import for Landing, Register, and Login, removed Switch from react-router import, added Provider import from react-redux and store from store.js, removed Switch routes and added routes for root, register, and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Books from "./pages/Books";
+// import Detail from "./pages/Detail";
 
-function App() {
-  return (
-    <Router>
-      <div>
+import Nav from "./components/Nav";
+import Landing from "./components/Landing";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
+// import NoMatch from "./pages/NoMatch";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+      <Router>
+      <div className="App">
         <Nav />
-        <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
+          <Route exact path="/" component={Landing} />
+          {/* <Route exact path="/books" component={Books} />
           <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch>
+          <Route exact path="/landing" component={Landing} /> */}
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          {/* <Route component={NoMatch} /> */}
       </div>
     </Router>
-  );
+    </Provider>
+    )
+  }
 }
 
 export default App;
