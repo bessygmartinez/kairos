5ff209fdad6b55e07f05680eddffb3e0269427a7
Bessy Gee <nakiwarai13@gmail.com>
2020-02-24 19:37:18 -0500

    added import for PropTypes, connect, loginUser, classnames, and checks for logged in account, changed className to classnames for error handling, added Login.propTypes and mapStateToProps, and changed export default

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../Actions/authActions";
+import classnames from "classnames";
+
 class Login extends Component {
   constructor() {
     super();
@@ -9,19 +14,47 @@ class Login extends Component {
       errors: {}
     };
   }
+
+  componentDidMount() {
+    //if logged in and user navigates to Login page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard"); //push user to dashboard when they login
+    }
+
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
+
 onChange = e => {
     this.setState({ [e.target.id]: e.target.value });
   };
+
 onSubmit = e => {
     e.preventDefault();
+
 const userData = {
       email: this.state.email,
       password: this.state.password
     };
-console.log(userData);
+
+this.props.loginUser(userData); //since we handle the redirect within our component, we don't need to pass
+//this.props.history as parameter
+
   };
+
 render() {
     const { errors } = this.state;
+
 return (
       <div className="container">
         <div style={{ marginTop: "3rem" }} className="row">
@@ -38,28 +71,34 @@ return (
             <form noValidate onSubmit={this.onSubmit}>
               <div className="form-group">
               <div className="input-field col-sm-12">
-                <label htmlFor="email">Email</label>
+                <label htmlFor="email">Email</label><br></br>
+                <span className="text-danger">{errors.email}{errors.emailnotfound}</span>
                 <input
                   onChange={this.onChange}
                   value={this.state.email}
                   error={errors.email}
                   id="email"
                   type="email"
-                  className="form-control"
+                  className={classnames("form-control", {
+                    invalid: errors.email || errors.emailnotfound
+                  }) }
                 />
                </div>
               </div>
 
               <div className="form-group">
               <div className="input-field col-sm-12">
-                <label htmlFor="password">Password</label>
+                <label htmlFor="password">Password</label><br></br>
+                <span className="text-danger">{errors.password}{errors.passwordincorrect}</span>
                 <input
                   onChange={this.onChange}
                   value={this.state.password}
                   error={errors.password}
                   id="password"
                   type="password"
-                  className="form-control"
+                  className={classnames("form-control", {
+                    invalid: errors.password || errors.passwordincorrect
+                  }) }
                 />
               </div>
               </div>
@@ -85,4 +124,19 @@ return (
     );
   }
 }
-export default Login;
+
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { loginUser }
+) (Login);
