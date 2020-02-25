4e5d2f6cf37ea6eb5800a0a38f9fe1587c4be4a8
Bessy Gee <nakiwarai13@gmail.com>
2020-02-24 19:29:53 -0500

    added import for jwt_decode, setAuthToken, authActions, PrivateRoute, Dashboard, check for log in/tokens, and added Switch Route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./Actions/authActions";
 // import Books from "./pages/Books";
 // import Detail from "./pages/Detail";
 
@@ -7,10 +10,33 @@ import Nav from "./components/Nav";
 import Landing from "./components/Landing";
 import Register from "./components/Auth/Register";
 import Login from "./components/Auth/Login";
+import PrivateRoute from "./components/Private-Route/PrivateRoute";
+import Dashboard from "./components/Dashboard/Dashboard";
 // import NoMatch from "./pages/NoMatch";
 
 import { Provider } from "react-redux";
 import store from "./store";
+
+//Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  //Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  //Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  //Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  //Check for expire token
+  const currentTime = Date.now() / 1000; //to get in milliseconds
+  if (decoded.exp < currentTime) {
+    //Logout user
+    store.dispatch(logoutUser());
+
+    //Redirect to login
+    window.location.href="./login";
+  }
+}
 
 
 class App extends Component {
@@ -26,6 +52,9 @@ class App extends Component {
           <Route exact path="/landing" component={Landing} /> */}
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
           {/* <Route component={NoMatch} /> */}
       </div>
     </Router>
