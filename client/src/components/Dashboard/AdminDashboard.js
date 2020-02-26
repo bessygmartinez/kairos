430d4e380f5176b9f4fb6584a14c0c1cfcc2554c
Bessy Gee <nakiwarai13@gmail.com>
2020-02-26 15:01:04 -0500

    copy and pasted script from Dashboard.js but changed component names

---
+++ client/src/components/Dashboard/AdminDashboard.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../Actions/authActions";
+
+class AdminDashboard extends Component {
+    onLogoutClick = e => {
+        e.preventDefault();
+        this.props.logoutUser();
+    };
+
+    render() {
+        const { user } = this.props.auth;
+
+    return (
+        <div style={{ marginTop: "2rem", height: "75vh" }} className="container valign-wrapper">
+            <div className="row">
+                <div className="col-sm-12 center-align">
+                    <h4>
+                        <b>Hey there,</b> {user.name.split(" ")[0]}
+                        <p className="flow-text text-secondary">
+                            You are logged into the {" "}
+                            <span style={{ fontFamily: "monospace"}}>ADMIN</span> dashboard.
+                        </p>
+                    </h4>
+                    <button style={{
+                        width: "150px",
+                        borderRadius: "3px",
+                        letterSpacing: "1.5px",
+                        marginTop: "1rem"
+                    }}
+                    onClick={this.onLogoutClick}
+                    className="btn btn-raised btn-large waves-effect waves-light hoverable teal-btn text-white">
+                        Logout
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+  }
+}
+
+AdminDashboard.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(
+    mapStateToProps,
+    { logoutUser }
+) (AdminDashboard);
