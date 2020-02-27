47e4ad3ca2436574d967e9aeab12e205a4c6f14d
Bessy Gee <nakiwarai13@gmail.com>
2020-02-26 19:51:11 -0500

    chanaged ExtractJwt to fromAuthHeaderWithScheme('jwt')

--- config/passport.js
+++ config/passport.js
@@ -5,10 +5,10 @@ const User = mongoose.model("users");
 const keys = require("../config/keys");
 
 const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
 opts.secretOrKey = keys.secretOrKey;
 
-module.exports = passport => {
+module.exports = (passport) => {
     passport.use(
         new JwtStrategy(opts, (jwt_payload, done) => {
             User.findById(jwt_payload.id)
