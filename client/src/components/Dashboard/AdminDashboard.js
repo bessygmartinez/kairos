12e35c6cff82118da82518234e9c725c7f66361b
Bessy Gee <nakiwarai13@gmail.com>
2020-02-29 11:09:38 -0500

    added Moment import, added Link, added greeting script, updated greeting to show, and added Register button

--- client/src/components/Dashboard/AdminDashboard.js
+++ client/src/components/Dashboard/AdminDashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import Moment from "react-moment";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { logoutUser } from "../../Actions/authActions";
 
 class AdminDashboard extends Component {
@@ -12,17 +14,51 @@ class AdminDashboard extends Component {
     render() {
         const { user } = this.props.auth;
 
+        //For greeting:
+        const today = new Date()
+        const curHr = today.getHours()
+        let greeting = "";
+
+        if (curHr < 12) {
+            greeting = "Good morning";
+        } else if (curHr < 17) {
+            greeting = "Good afternoon";
+        } else {
+            greeting = "Good evening";
+        }
+
     return (
         <div style={{ marginTop: "2rem", height: "75vh" }} className="container valign-wrapper">
             <div className="row">
+                <div className="col-sm-12 text-right">
+                <Moment interval={1000} format="LLLL" />
+                </div>
+            </div>
+            <div className="row">
                 <div className="col-sm-12 center-align">
                     <h4>
-                        <b>Hey there,</b> {user.name.split(" ")[0]}
+                        <b>{greeting}</b>, {user.name.split(" ")[0]}
                         <p className="flow-text text-secondary">
                             You are logged into the {" "}
                             <span style={{ fontFamily: "monospace"}}>ADMIN</span> dashboard.
                         </p>
                     </h4>
+
+                <div className="col-sm-12">
+                <Link
+                  to="/register"
+                  style={{
+                    width: "140px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px"
+                  }}
+                  className="btn btn-raised btn-large waves-effect waves-light hoverable teal-btn text-white"
+                >
+                  Register
+                </Link>
+              </div>
+
+              <div className="col-sm-12">
                     <button style={{
                         width: "150px",
                         borderRadius: "3px",
@@ -33,6 +69,8 @@ class AdminDashboard extends Component {
                     className="btn btn-raised btn-large waves-effect waves-light hoverable teal-btn text-white">
                         Logout
                     </button>
+                </div>
+                
                 </div>
             </div>
         </div>
