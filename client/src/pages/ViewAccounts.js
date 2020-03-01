578dd6f1de042b8fde7b35d663ee37e757bbb7a4
Bessy Gee <nakiwarai13@gmail.com>
2020-03-01 15:01:18 -0500

    added initial script for displaying users

---
+++ client/src/pages/ViewAccounts.js
@@ -0,0 +1,134 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Moment from "react-moment";
+import usersAPI from "../utils/usersAPI";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { logoutUser } from "../Actions/authActions";
+import ManagerDashboard from "../components/Dashboard/ManagerDashboard";
+import EmployeeDashboard from "../components/Dashboard/EmployeeDashboard";
+
+class ViewAccounts extends Component {
+    state = {
+        users: [],
+        name: "",
+        email: "",
+        role: ""
+    }
+
+    componentDidMount() {
+        this.loadUsers();
+    }
+
+    loadUsers = () => {
+        usersAPI.getUsers()
+        .then(res =>
+            this.setState({
+                users: res.data,
+                name: "",
+                email: "",
+                role: ""
+            })
+            )
+            .catch(err => console.log(err));
+    };
+
+    deleteUser = id => {
+        usersAPI.deleteUser(id)
+        .then(res => this.loadUsers())
+        .catch(err => console.log(err));
+    };
+
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
+  render() {
+    const { user } = this.props.auth;
+
+    if (user.role === "manager") {
+      return (
+        <div>
+          <ManagerDashboard />
+        </div>
+      );
+    } else if (user.role === "employee") {
+      return (
+        <div>
+          <EmployeeDashboard />
+        </div>
+      );
+    } else {
+      return (
+        <div style={{ marginTop: "2rem", height: "75vh" }} className="container">
+          <div className="row">
+            <div className="col-sm-12 text-right">
+              <Moment interval={1000} format="LLLL" />
+            </div>
+          </div>
+
+          <div className="row mt-5 text-center">
+            <div className="col-sm-12">
+              <h4>
+                <p className="flow-text text-secondary">
+                  Please see <span style={{ fontFamily: "monospace" }}>USERS</span> list below.
+                </p>
+              </h4>
+            </div>
+          </div>
+
+          <div className="row mt-4">
+            <div className="col-sm-12 text-center">             
+                 {this.state.users.map(user => (
+                     <Link to={"/users/" + user._id}><h2>{user.name}</h2></Link>
+                 ))}
+              
+            </div>
+
+            {/* <div className="col-sm-12 text-center mt-4">
+              <p>View all accounts to edit/delete:</p>
+              <Link
+                to="/"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-raised btn-large waves-effect waves-light hoverable teal-btn text-white"
+              >
+                View All
+              </Link>
+            </div> */}
+          </div>
+
+          {/* <div className="row mt-5">
+              <div className="col-sm-12">
+                    <button style={{
+                        width: "150px",
+                        borderRadius: "3px",
+                        letterSpacing: "1.5px",
+                        marginTop: "1rem"
+                    }}
+                    onClick={this.onLogoutClick}
+                    className="btn btn-raised btn-large waves-effect waves-light hoverable teal-btn text-white">
+                        Logout
+                    </button>
+                </div>
+                </div> */}
+        </div>
+      );
+    }
+  }
+}
+
+ViewAccounts.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logoutUser })(ViewAccounts);
