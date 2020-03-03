9169d05b1129dda82baf5c9ae5b4d33712515f8f
Bessy Gee <nakiwarai13@gmail.com>
2020-03-03 18:03:41 -0500

    added deleteUser funcetion, added back arrow link

--- client/src/pages/UserDetail.js
+++ client/src/pages/UserDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import usersAPI from "../utils/usersAPI";
+import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 
@@ -16,6 +17,12 @@ class userDetail extends Component {
       .catch(err => console.log(err));
   }
 
+  deleteUser = id => {
+    usersAPI.deleteUser(id)
+    .then(toast.error("Account has been deleted"))
+    .catch(err => console.log(err));
+};
+
   render() {
     let role = "";
 
@@ -30,12 +37,15 @@ class userDetail extends Component {
     return (
         <div style={{ marginTop: "2rem", height: "75vh" }} className="container">
           <div className="row">
-            <div className="col-sm-12 text-right">
+          <div className="col-sm-6 text-left">
+              <Link to="/"><i className="material-icons" style={{fontSize: "130%"}}>keyboard_backspace</i> back</Link>
+              </div>
+            <div className="col-sm-6 text-right">
               <Moment interval={1000} format="LLLL" />
             </div>
           </div>
 
-          <div className="row mt-5 text-center">
+          <div className="row mt-3 text-center">
           <div className="col-sm-12 col-lg-6 offset-lg-3">
             <div className="card border-secondary mb-3">
                 <h1 className="card-header">{this.state.user.name}</h1>
@@ -55,12 +65,15 @@ class userDetail extends Component {
                   }}>Edit <i className="material-icons" style={{fontSize: "130%"}}>edit</i></Link>
               </div>
               <div className="col">
-              <Link to="/" className="btn btn-sm bg-danger text-white waves-effect waves-light hoverable" style={{
+              <Link to="/" className="btn btn-sm bg-danger text-white waves-effect waves-light hoverable"
+                    style={{
                     width: "100px",
                     borderRadius: "3px",
                     letterSpacing: "1.5px",
                     marginTop: "1rem"
-                  }}>Delete <i className="material-icons" style={{fontSize: "130%"}}>delete_forever</i></Link>
+                    }}
+                    onClick={() => this.deleteUser(this.state.user._id)}
+                  >Delete <i className="material-icons" style={{fontSize: "130%"}}>delete_forever</i></Link>
               </div>
               </div>
               </div>
