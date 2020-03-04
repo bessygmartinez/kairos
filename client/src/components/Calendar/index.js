cac1530a12720e51d4c1d6dcce718fcbabb36797
Stephen Caceres <54186983+stephenc93@users.noreply.github.com>
2020-03-04 18:44:10 -0500

    added selectable functionality to Manager and Employee Calendar, only adds events to state

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -3,82 +3,71 @@ import moment from 'moment';
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const localizer = momentLocalizer(moment);
+const propTypes = {}
+
+const localizer = momentLocalizer(moment)
 
 class MyCalendar extends Component {
     constructor() {
-      super();
-      const formats = {
-        dateFormat: "DD",
-        dayRangeHeaderFormat: ({ start, end }, culture, localizer) =>
-          localizer.format(start, { date: "YYYY-MM-DD" }, culture) + ' — ' +
-          localizer.format(end, { date: "YYYY-MM-DD" }, culture)
-      };
+      super()
       const events = [
-        {
-          id: 0,
-          title: "Stephen - Unavailable",
-          allDay: true,
-          start: new Date("2020-03-01"),
-          end: new Date("2020-03-25"),
-        },
-        {
-          id: 0,
-          title: "Stephen - Available",
-          allDay: true,
-          start: new Date("2020-03-25"),
-          end: new Date("2020-03-31")
+        { 
+            id: 0,
+            title: "Stephen - Unavailable",
+            allDay: true,
+            start: new Date("2020-03-02"),
+            end: new Date("2020-03-25")
         },
         {
           id: 1,
-          title: "Bessy - Available",
+          title: "Stephen - Available",
           allDay: true,
-          start: new Date("2020-03-15"),
-          end: new Date("2020-03-15")
-        },
-        {
-          id: 2,
-          title: "Paola - Available",
-          allDay: true,
-          start: new Date("2020-03-25"),
-          end: new Date("2020-03-25")
-        },
-        {
-          id: 3,
-          title: "Paola - Available",
-          allDay: true,
-          start: new Date("2020-04-04"),
+          start: new Date("2020-04-01"),
           end: new Date("2020-04-04")
         }
       ]
+      const handleSelect = ({ start, end }) => {
+        const title = window.prompt('New Event name')
+        if (title)
+          this.setState({
+            events: [
+              ...this.state.events,
+              {
+                start,
+                end,
+                title,
+              },
+            ],
+          })
+      }    
       this.state = {
-        name: 'React',
         events,
-        formats
+        handleSelect
       };
     }
-  
+
     render() {
       return (
         <div>
           <div style={{ height: "500px", width: "1000px" }}>
             <Calendar
+              popup
               selectable
+              localizer={localizer}
               events={this.state.events}
-              formats={this.state.formats}
-              startAccessor="start"
-              endAccessor="end"
               views={["week", "month"]}
               defaultDate={moment().toDate()}
-              localizer={localizer}
               onSelectEvent={event => alert(event.title)}
-              onSelectSlot={this.handleSelect}
+              onSelectSlot={this.state.handleSelect}
             />
           </div>
         </div>
       );
     }
 }
+
+MyCalendar.propTypes = propTypes
+
 export default MyCalendar;
 
 
