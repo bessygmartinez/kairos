c138e5518e9a20c44437f1ec3c33996cceeda60c
Bessy Gee <nakiwarai13@gmail.com>
2020-03-09 15:27:40 -0400

    added dummy events for testing, changed format formatting for dates, added if statements for calendar selections, removed formatDate function, added event to parameters for showModa, removed availability from setState and added event, changed onSelectEvent to handle event and onSelectSlot to handle slotInfo, added id 'TheCalendar'

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,101 +1,120 @@
-import React, { Component } from 'react';
-import moment from 'moment';
+import React, { Component } from "react";
+import moment from "moment";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import Modal from "../Modal";
-import "./calendar.css"
+ 
+import "./calendar.css";
 
-const propTypes = {}
+const propTypes = {};
 
 const localizer = momentLocalizer(moment);
 
 class MyCalendar extends Component {
-    constructor() {
-      super();
+  constructor() {
+    super();
 
-      const events = [
-        { 
+    const events = [
+      {
         id: 0,
-        title: "",
-        start: "2020-03-02",
-        end: "2020-03-25",
+        title: "You",
+        start: new Date("2020/03/02"),
+        end: new Date("2020/03/02"),
         allDay: true,
         availability: true
-        }
-      ];
-      
-      const handleSelect = (event) => {
-        console.log(event.start)
-        let startDate = moment(event.start).format("dddd, MM-DD");
-        console.log(startDate);
-        let endDate = moment(event.end).format("dddd, MM-DD");
-        console.log(endDate)
+      },
+      {
+        id: 1,
+        title: "Me",
+        start: new Date("2020/03/13"),
+        end: new Date("2020/03/13"),
+        allDay: true,
+        availability: true
+      },
+      {
+        id: 2,
+        title: "Us",
+        start: new Date("2020/03/25"),
+        end: new Date("2020/03/25"),
+        allDay: true,
+        availability: false
+      },
+      {
+        id: 3,
+        title: "Them",
+        start: new Date("2020/03/16"),
+        end: new Date("2020/03/16"),
+        allDay: true,
+        availability: false
+      }
+    ];
 
-        this.showModal(startDate, endDate, true)
+    const handleSelect = event => {
+      let startDate = moment(event.start).format("dddd, MMMM DD, YYYY");
+      console.log(startDate);
 
-          this.setState({
-            events: [
-              ...this.state.events,
-              {
-                ...this.event,
-                availability: this.state.events.availability
-              },
-            ],
-          })
-      }    
-      this.state = {
-        events,
-        event: null,
-        handleSelect,
-        show: false,
+      let endDate = moment(event.end).format("dddd, MMMM DD, YYYY");
+      console.log(endDate);
+
+      console.log(event);
+
+      if (event.slots) {
+        this.showModal(event, startDate, endDate);
+      }
+
+
+      if (event.allDay) {
+        this.showModal(event, startDate, endDate);
+      }
+    };
+    
+    this.state = {
+      events,
+      event: null,
+      handleSelect,
+      show: false
       };
+    
+  }
 
-    }
+  showModal = (event, startDate, endDate) => {
+    this.setState({
+      show: !this.state.show,
+      event: event,
+      start: startDate,
+      end: endDate
+    });
+  };
 
-    formatDate = (event) => {
-      console.log(event)
-      // this.showModal(event)
+  render() {
+    return (
+      <div>
+        <div style={{ height: "500px", width: "1000px" }}>
+          <Modal
+            onClose={this.showModal}
+            show={this.state.show}
+            event={this.state.event}
+            start={this.state.start}
+            availability={event => this.state.event.availability}
+          />
 
-    }
-
-    showModal = (startDate, endDate, availability) => {
-      this.setState({
-        show: !this.state.show,
-        start: startDate,
-        end: endDate,
-        availability: availability
-      })
-      };
-
-    render() {
-
-      return (
-        <div>
-          <div style={{ height: "500px", width: "1000px" }}>
-
-            <Modal onClose={this.showModal} show={this.state.show} event={this.state.event} start={this.state.start}
-            availability={this.state.events.availability} />
-
-            <Calendar
-              popup
-              selectable
-              localizer={localizer}
-              events={this.state.events}
-              views={["week", "month"]}
-              defaultDate={moment().toDate()}
-              onSelectEvent={event => this.formatDate(event)}
-              onSelectSlot={event => this.state.handleSelect(event)}
-              modalAvailability={this.state.modalAvailability}
-            />
-          </div>
-          
+          <Calendar
+            popup
+            selectable
+            localizer={localizer}
+            events={this.state.events}
+            views={["week", "month"]}
+            defaultDate={moment().toDate()}
+            onSelectEvent={event => this.state.handleSelect(event)}
+            onSelectSlot={slotInfo => this.state.handleSelect(slotInfo)}
+            id="TheCalendar"
+          />
         </div>
-      );
-    }
+      </div>
+    );
+  }
 }
 
-MyCalendar.propTypes = propTypes
+MyCalendar.propTypes = propTypes;
 
 export default MyCalendar;
-
-
