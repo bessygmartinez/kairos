9665d653b0f3eaec6c2dd578a0ee425201bb736f
Bessy Gee <nakiwarai13@gmail.com>
2020-03-09 15:23:02 -0400

    removed react-redux import and moment, added componentWillReceiveProps function to handle toggle display, removed modalAvailability from state, removed Modal.propTypes and mapStatetoProps and connect (unecessary to share state of modal. each modal render should have it's own local state

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -1,30 +1,34 @@
 import React from "react";
 import "./modal.css";
-import moment from 'moment';
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-
-// const localizer = momentLocalizer(moment);
 
 class Modal extends React.Component {
-    state = {
-        switch: true,
-        modalAvailability: true
+  constructor() {
+    super();
+    this.state = {
+      switch: ""
+    };
+  }
+
+  UNSAFE_componentWillReceiveProps(nextprops) {
+    if (nextprops.event.availability === false) {
+      this.setState({ switch: false });
+    } else {
+      this.setState({ switch: true });
     }
+  }
 
   onClose = e => {
+    e.persist();
     this.props.onClose && this.props.onClose(e);
   };
 
   onChange = e => {
-    this.setState({ 
-        switch: !this.state.switch,
-        modalAvailability: !this.state.switch
-     });
+    this.setState({
+      switch: !this.state.switch
+    });
   };
 
   render() {
-    console.log(this.props)
     if (!this.props.show) {
       return null;
     }
@@ -32,52 +36,44 @@ class Modal extends React.Component {
     var buttons = document.querySelectorAll(".toggle-button");
     var modal = document.querySelector("#modal1");
 
-[].forEach.call(buttons, function(button) {
-  button.addEventListener("click", function() {
-    modal.classList.toggle("off");
-  })
-});
+    [].forEach.call(buttons, function(button) {
+      button.addEventListener("click", function() {
+        modal.classList.toggle("off");
+      });
+    });
 
     return (
-
-        <div className="modal-container">
-        <div className="modal1 fade" id="modal1">
-        <h2>{this.props.start}</h2>
-        <div className="content">
+      <div className="modal-container">
+        <div className="modal1" id="modal1">
+          <h2>{this.props.start}</h2>
+          <div className="content">
             <form>
-                <div className="switch">
-                    <label className="text-secondary">
-                        <input
-                        type="checkbox"
-                        checked={this.state.switch}
-                        onChange={this.onChange}
-                        value={this.state.switch}
-                        id="switch" />
-                        Available
-                    </label>
-                </div>
+              <div className="switch mt-3">
+                <label className="text-secondary">
+                  <input
+                    type="checkbox"
+                    onChange={this.onChange}
+                    checked={this.state.switch}
+                    value={this.state.switch}
+                    id="switch"
+                  />
+                  Available
+                </label>
+              </div>
             </form>
-            </div>
-        <div className="actions">
-          <button className="btn btn-raised btn-large waves-effect waves-light hoverable teal-btn text-white toggle-button ml-3 mb-3" onClick={this.onClose}>
-            Submit
-          </button>
+          </div>
+          <div className="actions">
+            <button
+              className="btn btn-raised btn-large waves-effect waves-light hoverable teal-btn text-white toggle-button ml-3 mb-3"
+              onClick={this.onClose}
+            >
+              Submit
+            </button>
+          </div>
         </div>
-      </div>
       </div>
     );
   }
 }
 
-Modal.propTypes = {
-    switch: PropTypes.bool,
-    modalAvailability: PropTypes.bool
-};
-
-const mapStateToProps = state => ({
-    switch: state.switch,
-});
-
-export default connect(
-    mapStateToProps,
-)(Modal);
+export default Modal;
