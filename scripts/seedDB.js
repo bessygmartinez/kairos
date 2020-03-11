5cca434f6b6526fa792b6bf93f1e703c745bd6d4
Bessy Gee <nakiwarai13@gmail.com>
2020-03-11 17:07:33 -0400

    added seeding function for dummy data

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -6,37 +6,40 @@ const db = require("../models");
 
 mongoose.connect(key);
 
-const workdaysSeed = [
+const createWorkdaysSeed = function(id, event) {
+  return db.Workday.create(event).then(docEvent => {
+    console.log("\n>> Created event:\n", docEvent);
+
+    return db.User.findByIdAndUpdate('5e6811825efe94704c7ef80b', {$push: {workday: docEvent._id}}, {new: true, useFindAndModify: false});
+  })
+}
+
+createWorkdaysSeed('5e6811825efe94704c7ef80b',
   {  
     title: "Dre",
     start: "2020-03-02",
     end: "2020-03-02",
     allDay: true,
     availability: false
-  },
-  {
+  }
+)
+
+createWorkdaysSeed('5e6811825efe94704c7ef80b',
+{
     title: "Dre",
     start: "2020-03-31",
     end: "2020-03-31",
     allDay: true,
     availability: true
-  },
-  {
+  }
+)
+
+createWorkdaysSeed('5e6811825efe94704c7ef80b',
+{
     title: "Dre",
     start: "2020-03-18",
     end: "2020-03-18",
     allDay: true,
     availability: false
   }
-]
-
-db.User
-  .then(() => db.User.findByIdAndUpdate(("5e56c9317bd8760db89acae0", workdaysSeed))
-  .then(data => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+)
