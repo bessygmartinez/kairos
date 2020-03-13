ea77ce0939a0869d272b42a4499cb66ac1d5c026
Bessy Gee <nakiwarai13@gmail.com>
2020-03-12 20:05:13 -0400

    added workdaysAPI and toast imports, added modalAvail in handleSelect, added onSubmit, onClose, and onChange

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -5,18 +5,19 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import Modal from "../Modal";
 
+import workdaysAPI from "../../utils/workdaysAPI";
+import { toast } from "react-toastify";
+
 import "./calendar.css";
 
 const localizer = momentLocalizer(moment);
 
 class MyCalendar extends Component {
-
-  componentDidMount () {
+  componentDidMount() {
     this.setState({
       ...this.state,
       events: this.props.auth.user.workdays
-    })
-        
+    });
   }
   constructor() {
     super();
@@ -24,57 +25,95 @@ class MyCalendar extends Component {
     const events = [];
 
     const handleSelect = event => {
-
       let startView = moment(event.start).format("dddd, MMMM DD, YYYY");
       let startDate = moment(event.start).format("YYYY/MM/DD");
 
       let endView = moment(event.end).format("dddd, MMMM DD, YYYY");
       let endDate = moment(event.end).format("YYYY/MM/DD");
 
+      let modalAvail;
+
       if (event.slots) {
-        this.showModal(event, startDate, endDate, startView, endView);
+        modalAvail = true;
+        this.showModal(event, startDate, endDate, startView, endView, modalAvail);
       }
 
       if (event.allDay) {
-        this.showModal(event, startDate, endDate, startView, endView);
+        if (event.availability === false) {
+          modalAvail = false;
+          this.showModal(event, startDate, endDate, startView, endView, modalAvail);
+        } else {
+          modalAvail = true;
+          this.showModal(event, startDate, endDate, startView, endView, modalAvail);
+        }
       }
     };
-    
+
     this.state = {
       events,
       event: null,
       handleSelect,
       show: false
     };
-    
   }
 
-  showModal = (event, startDate, endDate, startView, endView) => {
+  showModal = (event, startDate, endDate, startView, endView, modalAvail) => {
     this.setState({
       show: !this.state.show,
       event: event,
       startView: startView,
       endView: endView,
       startDate: startDate,
-      endDate: endDate
+      endDate: endDate,
+      switch: modalAvail
     });
   };
 
+  onClose = e => {
+    this.onClose() && this.onClose(e);
+  };
+
+  onChange = e => {
+    this.setState({ switch: !this.state.switch });
+  };
+
+  onSubmit = () => {
+    const workdaysUpdate = {
+      title: this.props.auth.user.name,
+      availability: this.state.switch,
+      start: this.state.startDate,
+      end: this.state.endDate,
+      allDay: true
+    };
+
+    workdaysAPI
+      .saveWorkday(this.props.auth.user.id, workdaysUpdate)
+      .then(toast.success("Schedule has been updated"))
+      .then(
+        this.setState({
+          events: [...this.state.events, workdaysUpdate]
+        })
+      );
+  };
+
   render() {
-        
     return (
       <div>
         <div style={{ height: "500px", width: "1000px" }}>
-          {this.state.show ? <Modal
-            onClose={this.showModal}
-            onSubmit={this.onSubmit}
-            show={this.state.show}
-            event={this.state.event}
-            startDate={this.state.startDate}
-            endDate={this.state.endDate}
-            startView={this.state.startView}
-            endView={this.state.endView}
-          /> : null}
+          {this.state.show ? (
+            <Modal
+              onClose={this.showModal}
+              onSubmit={this.onSubmit}
+              show={this.state.show}
+              event={this.state.event}
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+              startView={this.state.startView}
+              endView={this.state.endView}
+              switch={this.state.switch}
+              onChange={this.onChange}
+            />
+          ) : null}
 
           <Calendar
             popup
@@ -93,13 +132,10 @@ class MyCalendar extends Component {
   }
 }
 
-MyCalendar.propTypes = {
-};
+MyCalendar.propTypes = {};
 
 const mapStateToProps = state => ({
-  auth: state.auth,
-  events: state.events,
-  availability: state.availability
+  auth: state.auth
 });
 
-export default connect(mapStateToProps) (MyCalendar);
+export default connect(mapStateToProps)(MyCalendar);
