84029390608a6278631aa4fc3d4c203627c717fc
Bessy Gee <nakiwarai13@gmail.com>
2020-03-14 13:18:51 -0400

    added findAll method, corrected findById method, corrected findByIdAndInsertWorkday method

--- controllers/workdaysController.js
+++ controllers/workdaysController.js
@@ -2,11 +2,20 @@ const db = require("../models");
 
 // Defining methods for the workdaysController
 module.exports = {
+  findAll: function(req, res) {
+    db.Workday
+    .find(req.query)
+    .populate("User")
+    .sort({ "name": 1 })
+    .then(dbModel => res.json(dbModel))
+    .catch(err=>res.status(422).json(err));
+  },
   findAllById: function(req, res) {
-    return db.User
-      .findById({ _id: req.params.id })
+    db.User
+      .findById(req.params.id)
       .populate("workday")
-      .then((docEvent) => {
+      .then(docEvent => {
+        res.json(docEvent)
       console.log("\n>> Grabbing all events:\n", docEvent);
         return db.User.find(
           { workday: docEvent }, 
@@ -15,7 +24,9 @@ module.exports = {
   },
   findByIdAndInsertWorkday: function(req, res) {
     return db.Workday
-      .create(req.body).then((docEvent) => {
+      .create(req.body)
+      .then((docEvent) => {
+        res.json(docEvent)
         console.log("\n>> Created event:\n", docEvent);
         return db.User.findByIdAndUpdate(
           req.params.id, 
