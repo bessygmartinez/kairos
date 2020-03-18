472d8e671becc521a7620f4bd5f0336c5f81826b
Bessy Gee <nakiwarai13@gmail.com>
2020-03-18 15:56:41 -0400

    added x button to modal

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -3,10 +3,9 @@ import "./modal.css";
 import { connect } from "react-redux";
 
 class Modal extends React.Component {
-
   componentDidMount() {
-    this.setState({ switch: this.props.switch})
-    }
+    this.setState({ switch: this.props.switch });
+  }
 
   //   if (this.props.event.availability === false){
   //   this.setState({switch: this.props.event.availability})
@@ -21,25 +20,31 @@ class Modal extends React.Component {
   // }
 
   render() {
-      let button = document.getElementById("modal-button");
-      let modal = document.querySelector("#modal1");
+    let button = document.getElementById("modal-button");
+    let modal = document.querySelector("#modal1");
 
-      if(button){
-        button.addEventListener("click", function() {
-          modal.classList.add("off")
-        })
-      };
+    if (button) {
+      button.addEventListener("click", function() {
+        modal.classList.add("off");
+      });
+    }
 
-      // [].forEach.call(buttons, function(button) {
-      //   button.addEventListener("click", function() {
-      //     modal.classList.toggle("off");
-      //   });
-      // });
+    // [].forEach.call(buttons, function(button) {
+    //   button.addEventListener("click", function() {
+    //     modal.classList.toggle("off");
+    //   });
+    // });
 
     return (
       <div className="modal-container">
         <div className="modal1" id="modal1">
-        <h2>{this.props.startView} - {this.props.endView}</h2>
+          <div className="col-12 mt-0">
+            <button type="button" className="close" aria-label="Close" onClick={this.props.onClose}>
+              <span aria-hidden="true" className="display-4">&times;</span>
+            </button><br></br>
+            <h2 className="mr-3 mt-0">{this.props.startView}</h2>
+          </div>
+
           <div className="content">
             <form>
               <div className="switch mt-3">
@@ -58,7 +63,8 @@ class Modal extends React.Component {
             </form>
           </div>
           <div className="actions">
-            <button id="modal-button"
+            <button
+              id="modal-button"
               className="btn btn-raised btn-large waves-effect waves-light hoverable teal-btn text-white toggle-button ml-3 mb-3"
               onClick={this.props.onSubmit}
             >
