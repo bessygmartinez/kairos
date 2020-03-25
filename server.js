a3637cc8df5fe0a6024aae90a1ef58d8ead03c26
Bessy Gee <nakiwarai13@gmail.com>
2020-03-25 15:37:22 -0400

    changed static server

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ require("./config/passport")(passport);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static("client/build"));
 }
 
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"), function(err) {
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"), function(err) {
       if (err) {
         res.status(500).send(err)
       }
