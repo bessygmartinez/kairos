e0841c6c587f97d008da6e0d7d84e0617bffa5ce
Bessy Gee <nakiwarai13@gmail.com>
2020-03-25 14:50:49 -0400

    changed app.get to serve static assets

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
   app.get("/*", function(req, res) {
-    res.sendFile(path.join(`${__dirname}client/build/index.html`), function(err) {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"), function(err) {
       if (err) {
         res.status(500).send(err)
       }
