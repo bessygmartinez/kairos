5964fdad09d3ebb4e41c8e5cfb9da78a9d0d5fd4
Bessy Gee <nakiwarai13@gmail.com>
2020-03-26 13:08:16 -0400

    changed path.resolve to path.join

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("/*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, "./client/build/index.html"), function(err) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"), function(err) {
       if (err) {
         res.status(500).send(err)
       }
