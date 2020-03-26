3333db078945913e0374ad4ac20cde2281e06674
Bessy Gee <nakiwarai13@gmail.com>
2020-03-26 12:28:28 -0400

    added app.get again

--- server.js
+++ server.js
@@ -31,13 +31,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-  // app.get("*", function(req, res) {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"), function(err) {
-  //     if (err) {
-  //       res.status(500).send(err)
-  //     }
-  //   })
-  // })
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 
 // Add routes, both API and view
 app.use("/api/workdays", workdays);
