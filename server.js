a13e0315f2acad91d1050ee9285de0946e1df435
Bessy Gee <nakiwarai13@gmail.com>
2020-03-26 14:51:13 -0400

    changed static assets path

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ require("./config/passport")(passport);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(`${__dirname}../client/build/index.html`), function(err) {
+    res.sendFile(path.join(`${__dirname}./client/build/index.html`), function(err) {
       if (err) {
         res.status(500).send(err)
       }
